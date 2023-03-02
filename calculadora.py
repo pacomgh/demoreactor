27003bb32bfaf582910312b55978623ff85e383c
JhoneiroDev <jhoneiro12@hotmail.com>
2023-03-01 19:51:04 -0500

    Mi aporte

--- calculadora.py
+++ calculadora.py
@@ -5,5 +5,11 @@ def suma(numero1, numero2):
     total = numero1 + numero2
     return total
 
+def resta(numero1, numero2):
+    total = numero1 - numero2
+    return total
+
 resultado = suma(num1, num2)
-print('El resultado es: ',resultado)
+resultado2 = resta(num1, num2)
+
+print('El resultado es: ',resultado2)
