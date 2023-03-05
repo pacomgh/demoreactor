2b408a88ffc17d185acbc15fc69bd56dd4023f16
Elperrochicoo <seba1gonza@gmail.com>
2023-03-05 01:12:43 -0300

    aportazo

--- calculadora.py
+++ calculadora.py
@@ -1,6 +1,6 @@
 num1 = 5
 num2 = -2
-
+num3 =10
 def suma(numero1, numero2):
     total = numero1 * numero2
     return total
@@ -8,8 +8,12 @@ def suma(numero1, numero2):
 def resta(numero1, numero2):
     total = numero1 - numero2
     return total
+def suma(numero1, numero3):
+    total = numero1 + numero3
+    return total
 
 resultado = suma(num1, num2)
 resultado2 = resta(num1, num2)
+resultado3 = suma(num1, num3)
 
-print('El resultado es: ',resultado2)
+print('El resultado es: ', resultado2,resultado,resultado3)
