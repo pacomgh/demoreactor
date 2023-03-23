0403e7e51d06fa403372c6b8ae9ae1c63722326f
GOsorioB <german_osorio@outlook.com>
2023-03-23 17:44:21 -0500

    Mi Aporte GOsorioB

--- calculadora.py
+++ calculadora.py
@@ -5,11 +5,11 @@ def suma(numero1, numero2):
     total = numero1 * numero2
     return total
 
-def resta(numero1, numero2):
+def  resta(numero1, numero2):
     total = numero1 - numero2
     return total
 
 resultado = suma(num1, num2)
 resultado2 = resta(num1, num2)
 
-print('El resultado es: ',resultado2)
+print('El resultado 2 es: ',resultado2)
