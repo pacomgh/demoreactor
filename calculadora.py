fd3278b0809cf131e26f9fe1aafd18ff8d0a3dd1
Alexis G. Hernandez <halexisgustavo@gmail.com>
2023-03-25 11:17:17 -0600

    Mi aporte

--- calculadora.py
+++ calculadora.py
@@ -2,7 +2,7 @@ num1 = 5
 num2 = -2
 
 def suma(numero1, numero2):
-    total = numero1 * numero2
+    total = numero1 + numero2
     return total
 
 def resta(numero1, numero2):
@@ -13,3 +13,4 @@ resultado = suma(num1, num2)
 resultado2 = resta(num1, num2)
 
 print('El resultado es: ',resultado2)
+print('El resultado es: ',resultado)
